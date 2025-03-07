8a3b5060046e6e74099ab9adc59506d4b703cc75
jorabek710ly <jorabekabdurashidov555@gmail.com>
2025-03-07 10:26:19 +0500

    js

---
+++ src/js/script.js
@@ -0,0 +1,112 @@
+{ //======================================== masala 1
+    let a = 3
+    if (a > 0) {
+        console.log(a + 1);
+    }
+    if (a < 0) {
+        console.log(a);
+    }
+}
+{ // ==========================================masala 2
+    let a = -3
+    if (a > 0) {
+        console.log(a + 1);
+    }
+    if (a < 0) {
+        console.log(a - 2);
+    }
+}
+{ // =====================================masala 3
+    let a = 0
+    if (a > 0) {
+        console.log(a + 1);
+    }
+    if (a < 0) {
+        console.log(a - 2);
+    }
+    if (a === 0) {
+        console.log(a + 10);
+    }
+}
+{ //==================================== masala 4
+    let a = 5, b = 3, d = -3
+    let musbat = 0
+    if (a > 0) {
+        musbat++
+    }
+    if (b > 0) {
+        musbat++
+    }
+    if (d > 0) {
+        musbat++
+    }
+    console.log(musbat);
+
+}
+{ //===================================== masala 5
+    let a = 5, b = 3, d = -3
+    let musbat = 0
+    let manfiy = 0
+    if (a > 0) {
+        musbat++
+    }
+    if (b > 0) {
+        musbat++
+    }
+    if (d > 0) {
+        musbat++
+    }
+    if (a < 0) {
+        manfiy++
+    }
+    if (b < 0) {
+        manfiy++
+    }
+    if (d < 0) {
+        manfiy++
+    }
+    console.log(musbat, manfiy);
+
+}
+{ // ============================masala 6
+    let a = 5, b = 3
+
+    if (a > b) {
+        console.log(a, ": bu kotta ");
+
+    }
+    if (b > a) {
+        console.log(b, ": bu kotta ");
+    }
+
+}
+{ //=================================== masala 7
+    let a = 5, b = 3
+
+    if (a < b) {
+        console.log(a, ": bu kickina ");
+
+    }
+    if (b < a) {
+        console.log(b, ": bu kickina ");
+    }
+
+}
+{ // ==================================masala 8
+    let a = 2, b = 5
+
+    if (a > b) {
+        console.log(a, ": bu kotta ");
+
+    }
+    if (b > a) {
+        console.log(b, ": bu kotta ");
+    }
+    if (a < b) {
+        console.log(a, ": bu kickina ");
+
+    }
+    if (b < a) {
+        console.log(b, ": bu kickina ");
+    }
+}
